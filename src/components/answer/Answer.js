5e6cdadb79a0787d4bd010f37fa75764a9ae2c17
kit.perez <kit.perez@gmail.com>
2017-09-16 12:05:44 +1000

    add answer component

---
+++ src/components/answer/Answer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Answer.css';
+
+
+class Answer extends Component {
+
+  render() {
+    return <div>
+      <p>_</p>
+    </div>
+  }
+}
+
+export default Answer
