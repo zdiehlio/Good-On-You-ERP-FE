b378b291e3f7bb22b40574fb8009640c88ae8ab4
Danny Li <dannyli0109@gmail.com>
2017-09-16 11:33:58 +1000

    create component folder

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <select>
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="mercedes">Mercedes</option>
-          <option value="audi">Audi</option>
-        </select>
+        
       </div>
     );
   }
