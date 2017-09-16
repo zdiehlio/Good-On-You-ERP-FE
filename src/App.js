1fac042bc0e5a04f71ba60cb5e549777637e0264
Danny Li <dannyli0109@gmail.com>
2017-09-16 10:58:11 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <select>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default App;
