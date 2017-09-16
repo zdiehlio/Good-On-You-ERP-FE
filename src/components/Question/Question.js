fa9b24db630eacbe840f45affe128acd64ae5018
Danny Li <dannyli0109@gmail.com>
2017-09-16 13:27:00 +1000

    checkbox

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Question.css"
 
 
-export default function Question() {
+export default function Question(props) {
   var style = {
     width: "100%"
   }
@@ -10,9 +10,9 @@ export default function Question() {
       <div className="card">
         <div className="container">
           <h4><b>Which of the following standards systems is the brand compliant with?</b></h4>
-          <p>kdasjkdj</p>
-          <p>kdasjkdj</p>
-          <p>kdasjkdj</p>
+            <input type="checkbox" name="vehicle" value="Bike"/> I have a bike<br />
+            <input type="checkbox" name="vehicle" value="Car"/> I have a car<br />
+            <input type="submit" value="Save"/>
         </div>
       </div>
     );
