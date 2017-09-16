9b7265262ca28f4180f2d3d06772cb9e0d614102
Danny Li <dannyli0109@gmail.com>
2017-09-17 08:29:10 +1000

    categoried

---
+++ src/backup.js
@@ -0,0 +1,23 @@
+{
+  "issueNumber": 1.1,
+  "question": "1 - Which of the following standards systems is the brand compliant with?",
+  "answers":
+  [
+    "Cradle to Cradle Platinum",
+    "Cradle to Cradle Gold",
+  ],
+  "appliesTo": "All",
+  "chooseAllThatApply": true,
+  "score": 100
+},{
+  "issueNumber": 1.2,
+  "question": "2 - Which of the following standards systems is the brand compliant with?",
+  "answers":
+  [
+    "Cradle to Cradle Platinum",
+    "Cradle to Cradle Gold",
+  ],
+  "appliesTo": "All",
+  "chooseAllThatApply": true,
+  "score": 100
+}
