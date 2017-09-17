e93ce333b2931b554a657bf5f95919462c202a30
kit.perez <kit.perez@gmail.com>
2017-09-17 10:39:59 +1000

    updated Question component to use Material-ui Checkbox

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import Checkbox from 'material-ui/Checkbox';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {teal900} from 'material-ui/styles/colors';
 import "./Question.css"
 
+const styles = {
+  block: {
+    maxWidth: 250,
+  },
+  checkbox: {
+    marginBottom: 16,
+  },
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: teal900
+  }
+});
 
 export default function Question(props) {
+
   var style = {
     alignSelf: "flex-end"
   }
@@ -14,7 +33,17 @@ export default function Question(props) {
   // })
 
   var answers = props.answers.map((ele, i) =>
-    <span key={i}><input key={i} onChange={props.handleSelectionChange} type="checkbox" name="vehicle" value="Car"/>{ele.text}</span>
+    // <span key={i}><input key={i} onChange={props.handleSelectionChange} type="checkbox" name="vehicle" value="Car"/>{ele.text}</span>
+
+    <span key={`q${props.currentQuestion}-res${i}`}>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <Checkbox
+              label={ele.text}
+              onCheck={props.handleSelectionChange}
+              style={styles.checkbox}
+            />
+      </MuiThemeProvider>
+    </span>
   )
     return (
       <div className="card">
