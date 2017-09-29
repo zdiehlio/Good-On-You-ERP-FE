37277b88669e02e6664154ad62ed978d776a4859
Danny Li <dannyli0109@gmail.com>
2017-09-29 16:53:11 +1000

    create brand sumbit handler

--- src/pages/createBrand/CreateBrand.js
+++ src/pages/createBrand/CreateBrand.js
@@ -4,6 +4,8 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {grey900} from 'material-ui/styles/colors';
 import './CreateBrand.css';
+import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux';
 
 const muiTheme = getMuiTheme({
   palette: {
@@ -11,31 +13,76 @@ const muiTheme = getMuiTheme({
   }
 });
 
+// <MuiThemeProvider muiTheme={muiTheme}>
+// <TextField
+//   hintText="brand name"
+//   floatingLabelText="Brand Name"
+// /></MuiThemeProvider>
+//
+// <MuiThemeProvider muiTheme={muiTheme}>
+// <TextField
+//   hintText="http://www.brand.url"
+//   floatingLabelText="Brand URL"
+// /></MuiThemeProvider>
+//
+// <MuiThemeProvider muiTheme={muiTheme}>
+// <TextField
+//   hintText="abc@email.com"
+//   floatingLabelText="Contact Email"
+// /></MuiThemeProvider>
+// <br />
+// <button className="button"><span>GO</span></button>
+
+
+
 class CreateBrand extends Component {
+
+  renderField(field) {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <TextField
+            floatingLabelText={field.label}
+            type={field.type}
+            {...field.input}
+          />
+        </MuiThemeProvider>
+      </div>
+    )
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
       <div className="page-container">
         <h2 className="title">CreateBrand</h2>
         <div className="form-container">
-          <MuiThemeProvider muiTheme={muiTheme}>
-          <TextField
-            hintText="brand name"
-            floatingLabelText="Brand Name"
-          /></MuiThemeProvider>
-
-          <MuiThemeProvider muiTheme={muiTheme}>
-          <TextField
-            hintText="http://www.brand.url"
-            floatingLabelText="Brand URL"
-          /></MuiThemeProvider>
-
-          <MuiThemeProvider muiTheme={muiTheme}>
-          <TextField
-            hintText="abc@email.com"
-            floatingLabelText="Contact Email"
-          /></MuiThemeProvider>
-          <br />
-          <button className="button"><span>GO</span></button>
+          <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+            <Field
+              label="Brand Name"
+              name="name"
+              type="text"
+              component={this.renderField}
+            ></Field>
+            <Field
+              label="Brand URL"
+              name="url"
+              type="text"
+              component={this.renderField}
+            ></Field>
+            <Field
+              label="Brand Email"
+              name="email"
+              type="email"
+              component={this.renderField}
+            ></Field>
+            <button className="button">Submit</button>
+          </form>
         </div>
 
 
@@ -44,4 +91,6 @@ class CreateBrand extends Component {
   }
 }
 
-export default CreateBrand;
+export default reduxForm({
+  form: "CreateBrandForm"
+})(CreateBrand)
