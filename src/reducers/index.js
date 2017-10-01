49c8979ddbf83bd8f96a0e5231a75bb0220dc98d
Danny Li <dannyli0109@gmail.com>
2017-10-02 05:19:21 +1100

    get all users and brands

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import LoginReducer from "./reducer_login"
 import CreateBrandReducer from "./reducer_create_brand"
+import SearchUsersReducer from "./reducer_search_users"
+import SearchBrandsReducer from "./reducer_search_brands"
 
 const rootReducer = combineReducers({
   login: LoginReducer,
   createBrand: CreateBrandReducer,
+  searchUsers: SearchUsersReducer,
+  searchBrands: SearchBrandsReducer,
   form: formReducer
 });
 
