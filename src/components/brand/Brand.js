a7c88fcb6bf48ee5a587c720799dbee71592ee20
kit.perez <kit.perez@gmail.com>
2017-10-07 00:55:10 +1100

    changed from material-ui flatbutton to link component so that the brandName information can be passed

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -1,20 +1,8 @@
 import React from 'react'
 import './Brand.css';
 import { Link } from 'react-router-dom';
-import FlatButton from 'material-ui/FlatButton';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {teal400} from 'material-ui/styles/colors';
 
-const muiTheme = getMuiTheme({
-  palette: {
-    textColor: teal400
-  }
-});
-
-// <Link name={brand.name} onClick={brand.handleViewSummaryClick} to='/brandSummary' className='button' style={{width: "100px", marginTop: "20px"}}>view</Link>
 const Brand = (brand) => {
-
   const name = brand.name;
   const category = brand.category;
 
@@ -23,11 +11,7 @@ const Brand = (brand) => {
       <div className="brand-row list-item">
         <a className="brand-url">{name}</a>
         <p className="brand-category">{category}</p>
-        <MuiThemeProvider muiTheme={muiTheme}>
-          <FlatButton
-            containerElement={<Link to="/brandSummary" />}
-            label="view"/>
-        </MuiThemeProvider>
+        <Link name={brand.name} onClick={brand.handleViewSummaryClick} to='/brandSummary' className='landingButton' style={{width: "100px", height:"40px"}}>view</Link>
       </div>
     </div>
   )
