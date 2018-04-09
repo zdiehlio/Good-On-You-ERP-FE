f76a0efbde690447a9ec30185b78ca5a58871ece
zdiehlio <zachary.diehl@gmail.com>
2018-04-09 17:31:02 -0400

    adjusted style to price on brand page

--- src/components/price/index.js
+++ src/components/price/index.js
@@ -5,13 +5,13 @@ import { Icon } from 'semantic-ui-react'
 
 const Price = (props) => {
   return(
-    <span>
+    <span className='price'>
       {console.log('PRICE:', props.price)}
       {props.price >= 1 ? (<div><span>$</span></div>) : (<div><span className='gray-out'>$</span></div>)}
       {props.price >= 2 ? (<div><span>$</span></div>) : (<div><span className='gray-out'>$</span></div>)}
       {props.price >= 3 ? (<div><span>$</span></div>) : (<div><span className='gray-out'>$</span></div>)}
       {props.price >= 4 ? (<div><span>$</span></div>) : (<div><span className='gray-out'>$</span></div>)}
-  
+
       {/* { props.price >= 1 ? <Icon color='#5D5D5D' name='dollar fitted' /> : <Icon color='black' name='dollar fitted' disabled='true' /> }
       { props.price >= 2 ? <Icon color='#5D5D5D' name='dollar fitted' /> : <Icon color='black' name='dollar fitted' disabled='true' /> }
       { props.price >= 3 ? <Icon color='#5D5D5D' name='dollar fitted' /> : <Icon color='black' name='dollar fitted' disabled='true' /> }
